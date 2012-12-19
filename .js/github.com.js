2cf8c9c1c07fa9450eda21e00d1282297a186673
3rd-Eden <info@3rd-Eden.com>
2012-12-19 16:49:59 +0100

    [minor] Show package.json under the tree view, zomg handy.

--- .js/github.com.js
+++ .js/github.com.js
@@ -5,6 +5,7 @@
 $(function boostrap() {
   'use strict';
 
+  // merge automagically
   $('.merge-pr .mergeable. p.message').each(function each() {
     var message = $(this);
 
@@ -17,4 +18,30 @@ $(function boostrap() {
   if (~$('title').html().toLowerCase().indexOf('unicorn')) {
     $('.container p img').css('-webkit-transform', 'scaleX(-1)');
   }
+
+  // show package.json under the tree view
+  var match = $('#slider .tree-browser a').filter(function () {
+    return 'package.json' === $(this).text();
+  });
+
+  if (match.length) $.get(match.attr('href'), function (res) {
+    var file = $(res).find('#files');
+
+    // make it look good
+    file.css({
+        marginTop: -15
+      , marginBottom: 15
+      , display: 'none'
+    });
+
+    // append package.json title
+    file.find('.info span.icon').append($('<b>package.json</b>').css({
+        padding: '8px 4px'
+      , display: 'inline-block'
+    }));
+
+    // add it to the dom and animate it
+    $('.tree-browser-wrapper').after(file);
+    file.fadeIn();
+  });
 });
